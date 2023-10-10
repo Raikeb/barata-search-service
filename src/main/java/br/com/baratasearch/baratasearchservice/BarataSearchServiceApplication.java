650ad057423ba12ecd59bf102c7cdd3cc6342e67
Raike Peçanha Mendes Barata <Raike.mb@gmail.com>
2023-10-10 19:20:45 -0300

    Base inicial do projeto

---
+++ src/main/java/br/com/baratasearch/baratasearchservice/BarataSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.baratasearch.baratasearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarataSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarataSearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/baratasearch/baratasearchservice/BarataSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.baratasearch.baratasearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarataSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
