739c20b2c0128c8b090439dca642bed4e84780ed
Raike Peçanha Mendes Barata <Raike.mb@gmail.com>
2023-10-13 19:43:20 -0300

    scraping captando melhor voo com atributos dele

--- src/main/java/br/com/baratasearch/baratasearchservice/util/ScrapingUtil.java
+++ src/main/java/br/com/baratasearch/baratasearchservice/util/ScrapingUtil.java
@@ -40,14 +40,98 @@ public class ScrapingUtil {
 			document = Jsoup.connect(url).get();
 			
 			String title = document.title();
-			LOGGER.info("Titulo da página: {}", title);
+			LOGGER.info("Informações de melhor Voo {}", title);//titulo da página
 			
-		} catch (IOException e) {
+			obtemCompanhiaVoo(document);
+			obtemDuracaoVoo(document);
+			StatusVoo statusVoo =  obtemStatusVoo(document);
+			obtemCarbonoVoo(document);
+			obtemPrecoVoo(document);
+			
+			} catch (IOException e) {
 			LOGGER.error("Erro ao tentar conectar com o Google Flights usando JSOUP -> {} ", e.getMessage()) ;
 			e.printStackTrace();
 		}
 		
 		return voo;
 	}
+	
+	public String obtemCompanhiaVoo(Document document) {
+		String companhia = null;
+		
+		boolean isCompanhia = document.select("div[class=sSHqwe tPgKwe ogfYpf]").isEmpty();
+		if (!isCompanhia) {
+			companhia = document.select("div[class=sSHqwe tPgKwe ogfYpf]").first().text();			
+		}
+		LOGGER.info("Companhia aérea: {}", companhia);
+		
+		return companhia;
+		}
+	
+	public String obtemDuracaoVoo(Document document) {
+		String duracao = null;
+		
+		boolean isDuracao = document.select("div[class=gvkrdb AdWm1c tPgKwe ogfYpf]").isEmpty();
+		if (!isDuracao) {
+			duracao = document.select("div[class=gvkrdb AdWm1c tPgKwe ogfYpf]").first().text();			
+		}
+		LOGGER.info("Duracao total: {}", duracao);
+		
+		return duracao;
+		}
+	
+	public StatusVoo obtemStatusVoo(Document document) {
+		//		SEM_VOO,
+		//		SEM_ESCALAS,
+		//		SEM_ESCALAS_ATRASADO,
+		//		COM_ESCALAS,
+		//		COM_ESCALAS_ATRASADO;
+		
+		StatusVoo statusVoo = StatusVoo.SEM_VOO;
+		boolean averiguaStatus;
+		
+		boolean isStatusVoo = document.select("span[class=ogfYpf]").isEmpty();
+		if (!isStatusVoo) {
+			String pegaStatusVoo = document.select("span[class=ogfYpf]").first().text();
+			
+			if (averiguaStatus = pegaStatusVoo.contains("parada")) {
+				statusVoo = StatusVoo.COM_ESCALAS;
+			} else {
+				statusVoo = StatusVoo.SEM_ESCALAS;
+			}
+			LOGGER.info("Status: {}", pegaStatusVoo);
+			
+			
+		}
+		return statusVoo;
+	}
+	
+	
+	public String obtemCarbonoVoo(Document document) {
+		String carbono = null;
+		
+		boolean isCarbono = document.select("div[class=AdWm1c lc3qH ogfYpf  Jucbnc PtgtFe]").isEmpty();
+		if (!isCarbono) {
+			carbono = document.select("div[class=AdWm1c lc3qH ogfYpf  Jucbnc PtgtFe]").first().text();			
+		}
+		LOGGER.info("Emissão de carbono: {}", carbono);
+		
+		return carbono;
+	}
+	
+	public String obtemPrecoVoo(Document document) {
+		String preco = null;
+		
+		boolean isPreco = document.select("div[class=YMlIz FpEdX]").isEmpty();
+		if (!isPreco) {
+			preco = document.select("div[class=YMlIz FpEdX]").first().text();			
+		}
+		LOGGER.info("Preço: {}", preco);
+		
+		return preco;
+	}
+	
+	
+		
 
 }
